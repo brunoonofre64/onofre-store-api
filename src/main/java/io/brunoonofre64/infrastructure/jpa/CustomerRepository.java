84e62e148f5b0aaba42458ddfe5a4ce3227df9f7
bruno.onofre <bruno.onofre@spcbrasil.org.br>
2022-11-24 09:55:46 -0300

    deletando metodo nao usado no repositorio

--- src/main/java/io/brunoonofre64/infrastructure/jpa/CustomerRepository.java
+++ src/main/java/io/brunoonofre64/infrastructure/jpa/CustomerRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface CustomerRepository extends PagingAndSortingRepository<CustomerEntity, Long> {
     CustomerEntity findByUuid(String uuid);
-    Page<CustomerDTO> findAllBy(Pageable pageable);
     void deleteByUuid(String uuid);
     boolean existsByUuid(String uuid);
 }

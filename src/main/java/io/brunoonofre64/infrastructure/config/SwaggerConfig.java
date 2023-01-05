9c590a46282f7077027ea2d94be8f21f611e8126
onofre <brunoonofre64@gmail.com>
2023-01-05 15:29:16 -0300

    implementando swagger

---
+++ src/main/java/io/brunoonofre64/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package io.brunoonofre64.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.brunoonofre64.api.v1"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API Restful + Testes + Seguranca.")
+                .description("Sistema para gerenciar produtos e pessoas.")
+                .version("1.0.0")
+                .contact(new Contact("Bruno Roberto Onofre",
+                        "https://github.com/brunoonofre64",
+                        "brunoonofre64@gmail.com"
+                )).build();
+     }
+}

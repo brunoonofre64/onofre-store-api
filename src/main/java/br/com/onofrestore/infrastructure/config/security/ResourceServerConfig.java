f35cc22ab816f36f63caa510fd7bfc1b2fffd85a
onofre <brunoonofre64@gmail.com>
2023-02-20 14:23:00 -0300

    implementado decoder jwt

--- src/main/java/br/com/onofrestore/infrastructure/config/security/ResourceServerConfig.java
+++ src/main/java/br/com/onofrestore/infrastructure/config/security/ResourceServerConfig.java
@@ -4,7 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,9 +21,16 @@ public class ResourceServerConfig {
             .and()
                 .cors()
             .and()
-                .oauth2ResourceServer().opaqueToken();
+                .oauth2ResourceServer().jwt();
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var secretKey = new SecretKeySpec("apoksd92oaisdkpi28ajsndaosdn8923u3839qAOSID9".getBytes(), "HmacSHA256");
+
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 
 }
